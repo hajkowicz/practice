7e411570d26d8fd84ec4eac70655860dac765102
Alex Taylor <alta@supengge-mbp.dhcp.thefacebook.com>
2018-08-07 22:06:07 -0700

    Fix typescript imports and ignore *.js files

--- blackjack.ts
+++ blackjack.ts
@@ -2,27 +2,27 @@ var readline_sync = require("readline-sync");
 //var userName = readline_sync.question("What is your REAL name? ");
 //console.log("Hello " + userName + "!");
 
-var deck = require("./blackjackhelpers.js");
+import deck = require("./blackjackhelpers");
 
 console.log("GAME BEGIN")
 
 // 1. Show them their 2 cards and dealer 1 card
 
-var playerCards = [deck.getCard(), deck.getCard()] 
-var dealerCards = [deck.getCard(), deck.getCard()] 
+var playerCards = [deck.getCard(), deck.getCard()]
+var dealerCards = [deck.getCard(), deck.getCard()]
 console.log("You drew " + playerCards[0] + " and " + playerCards[1] + ". Your total is " + deck.getSum(playerCards) + ".");
 console.log("Dealer shows " + dealerCards[0]);
 
 // 2. Ask if they want to hit / stay
 
 while (deck.getSum(playerCards) < 21){
-	
+
 
 
 	var introQuestion = readline_sync.question("Would you like to stay or hit?");
 
-		// 3a. if hit, show them another card 
-		// 3b. if stay, dealer shows 2nd card 
+		// 3a. if hit, show them another card
+		// 3b. if stay, dealer shows 2nd card
 
 	if (introQuestion === "hit"){
 		playerCards.push(deck.getCard())
@@ -37,7 +37,7 @@ while (deck.getSum(playerCards) < 21){
 
 
 
-//Dealer's Turn 
+//Dealer's Turn
 
 var dealerSum = deck.getSum(dealerCards)
 var playerSum = deck.getSum(playerCards)
@@ -48,7 +48,7 @@ while (deck.getSum(dealerCards) < 17 && playerSum < 21){
 	var currentCard = deck.getCard()
 	dealerCards.push(currentCard)
 	console.log("The Dealer drew a " + currentCard + ".")
-	dealerSum = deck.getSum(dealerCards)	
+	dealerSum = deck.getSum(dealerCards)
 }
 
 if (playerSum > 21){
@@ -66,17 +66,15 @@ if (playerSum > 21){
 } else {
 	console.log("The dealer's total is " + dealerSum + ". YOU WIN!")
 }
-	
 
-	// 4a. Check if player busted 
-	// 4b. Check if dealer busted  
-	// 4c. Check if player got blackjack 
+
+	// 4a. Check if player busted
+	// 4b. Check if dealer busted
+	// 4c. Check if player got blackjack
 	// 4d. Check if dealer got blackjack
-	// 4e. Check if tied 
-	// 4f. Check if player sum > dealer sum 
-	// 5. Print win/lose 
+	// 4e. Check if tied
+	// 4f. Check if player sum > dealer sum
+	// 5. Print win/lose
 
-	//max 4 unique numbers 
-	//deal with Aces 
-
-
+	//max 4 unique numbers
+	//deal with Aces
--- blackjackhelpers.ts
+++ blackjackhelpers.ts
@@ -1,4 +1,4 @@
-var getCard = function(): number{
+export var getCard = function(): number{
 	return getRandomInt(1, 12);
 }
 
@@ -8,16 +8,13 @@ var getRandomInt = function(min: number, max: number): number{
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-var getSum = function(cards: Array<number>): number{
+export var getSum = function(cards: Array<number>): number{
 	var sum = 0
 
 	for (var i = 0; i < cards.length; i++){
 		sum = sum + cards[i]
 	}
-	
 
-	return sum 
+
+	return sum
 }
-
-module.exports.getCard = getCard;
-module.exports.getSum = getSum;
