d82090cf5342bb94a6bb7aa9ad65b36af83b61b1
knighthawk <ty.hack@gmail.com>
2018-08-09 20:50:56 -0700

    format with prettier

--- blackjackhelpers.ts
+++ blackjackhelpers.ts
@@ -1,80 +1,71 @@
+type Card = { suit: string; name: string; value: number };
 
+export var shuffle = function(a: Array<Card>): Array<Card> {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+};
 
+var getNameFromValue = function(num: number): string {
+  //input 1 , output "Ace" , input 2, output "two", etc ...
+  if (num >= 2 && num <= 10) {
+    return String(num);
+  } else if (num === 1) {
+    return "Ace";
+  } else if (num === 11) {
+    return "Jack";
+  } else if (num === 12) {
+    return "Queen";
+  } else {
+    return "King";
+  }
+};
 
-type Card = {suit: string, name: string, value: number};
+var getSuit = function(num: number): string {
+  var suitDescriptor = ["spades", "clubs", "hearts", "diamonds"];
+  return suitDescriptor[num];
+};
 
-export var shuffle = function(a: Array<Card>): Array<Card>{
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+export var getDeck = function(): Array<Card> {
+  var deck = [];
+
+  for (var y = 0; y < 4; y++) {
+    for (var x = 0; x < 13; x++) {
+      var cardOfSpades = {
+        suit: getSuit(y),
+        name: getNameFromValue(x + 1),
+        value: x + 1
+      };
+      deck.push(cardOfSpades);
     }
-    return a;
-}
+  }
+  return deck;
+};
 
-var getNameFromValue = function(num: number): string{
-	//input 1 , output "Ace" , input 2, output "two", etc ...
-	 if (num >= 2 && num <= 10) {
-	 	return String(num)
-	 } else if (num === 1){ 
-	 	return "Ace"
-	 } else if (num === 11){
-	 	return "Jack"
-	 } else if (num === 12){
-	 	return "Queen"
-	 } else {
-	 	return "King"
-	 }
-}
+export var drawCard = function(currentDeck: Array<Card>): Card {
+  var currentCard = currentDeck.pop();
+  if (currentCard === undefined) {
+    throw new Error("Deck is Empty.");
+  }
+  return currentCard;
+};
 
-var getSuit = function(num: number): string{
-	var suitDescriptor = ["spades", "clubs", "hearts", "diamonds"]
-	return suitDescriptor[num]
-}
-
-export var getDeck = function(): Array<Card>{
-	
-	var deck = [];
-
-	for (var y = 0; y < 4; y++){
-			
-
-		for (var x = 0; x < 13; x++){
-			var cardOfSpades = {suit: getSuit(y), name: getNameFromValue(x + 1), value: x + 1}
-			deck.push(cardOfSpades)
-		}
-	}
-	return deck
-}
-
-export var drawCard = function(currentDeck: Array<Card>): Card{
-	var currentCard = currentDeck.pop()
-	if (currentCard === undefined){
-		throw new Error("Deck is Empty.")
-	} 
-	return currentCard;
-
-
-}
-
-var getRandomInt = function(min: number, max: number): number{
+var getRandomInt = function(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+};
 
+export var getSum = function(cards: Array<Card>): number {
+  var sum = 0;
 
-
-export var getSum = function(cards: Array<Card>): number{
-
-
-	var sum = 0
-
-	for (var i = 0; i < cards.length; i++){
-		sum = sum + cards[i].value
-	}
-	return sum
-}
-
+  for (var i = 0; i < cards.length; i++) {
+    sum = sum + cards[i].value;
+  }
+  return sum;
+};
